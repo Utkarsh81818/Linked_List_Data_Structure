c0793a570254615bcbe0b66ca8e5ca4680a61c85
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-24 16:57:49 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/linkedlist/Main.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.linkedlist;
+/**
+ * Purpose : Simply printing the welcome message for data structure Programs.
+ * @author Utkarsh Mishra
+ * @since 2021/09/24
+ */
+
+/*
+Simply created the method for welcome message.
+ */
+public class Main {
+    public static void display(){
+        System.out.println("Welcome to \nData Structure\n" + "Problems using\n" + "Java Generics");
+    }
+    public static void main(String[] args) {
+        display();
+    }
+}
