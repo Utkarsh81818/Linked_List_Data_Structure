2efed6dde22cce45d5841dd8a9b9cf1804773745
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-24 17:34:00 +0530

    Created: Linked List for adding 3 elements

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.linkedlist;
 /**
- * Purpose : Simply printing the welcome message for data structure Programs.
+ * Purpose : We have to create a simple Linked List to store 3 values.
  * @author Utkarsh Mishra
  * @since 2021/09/24
  */
@@ -10,9 +10,22 @@ Simply created the method for welcome message.
  */
 public class LinkedList {
     public static void display(){
-        System.out.println("Welcome to \nData Structure\n" + "Problems using\n" + "Java Generics");
+        System.out.println("Welcome to Data Structure Problems using Java Generics");
     }
     public static void main(String[] args) {
         display();
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
