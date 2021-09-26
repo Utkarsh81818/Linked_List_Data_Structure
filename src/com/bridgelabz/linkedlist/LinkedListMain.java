a0585519ec49a1565d243b676b8e69f3d25a683a
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-26 12:59:32 +0530

    Added: Appending Data at End

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.linkedlist;
 /**
- * Purpose : We have to push data at starting.
+ * Purpose : We have to append 30 and 70 to 56.
  * @author Utkarsh Mishra
- * @since 2021/09/24
+ * @since 2021/09/26
  */
 import java.util.Scanner;
 
@@ -11,9 +11,14 @@ public class LinkedListMain {
         System.out.println("Welcome to linkedList DataStructure Program");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to add the data at the start");
+        System.out.println("Enter 2 to add the data at the end");
         switch (scanner.nextInt()) {
             case 1:
                 Operations.addDataAtStart();
+                break;
+            case 2:
+                Operations.addDataAtEnd();
+                break;
         }
     }
 }
